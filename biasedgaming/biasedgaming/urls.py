5d5a57d85a5695a80b63ad69a44509867c70c2cc
Jeff Schwaber <jschwaber@mobilepaks.com>
2014-01-24 23:38:16 -0800

    reformat staticpatterns

--- biasedgaming/biasedgaming/urls.py
+++ biasedgaming/biasedgaming/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, include, url
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 from django.contrib import admin
 admin.autodiscover()
@@ -9,5 +10,4 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
 )
 
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-    urlpatterns += staticfiles_urlpatterns()
+urlpatterns += staticfiles_urlpatterns()
