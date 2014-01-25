425f782f5bb04b7de82d025693eb8ecb73c28ee7
Jeff Schwaber <jschwaber@mobilepaks.com>
2014-01-24 23:47:43 -0800

    attempt at wsgi.py

--- biasedgaming/biasedgaming/urls.py
+++ biasedgaming/biasedgaming/urls.py
@@ -1,5 +1,4 @@
 from django.conf.urls import patterns, include, url
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 from django.contrib import admin
 admin.autodiscover()
@@ -10,4 +9,3 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
 )
 
-urlpatterns += staticfiles_urlpatterns()
