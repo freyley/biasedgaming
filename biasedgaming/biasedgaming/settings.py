7973be1adc50594a6a0818aaf00938c7b327bee3
Jeff Schwaber <jschwaber@mobilepaks.com>
2014-01-24 23:33:50 -0800

    allow the host

--- biasedgaming/biasedgaming/settings.py
+++ biasedgaming/biasedgaming/settings.py
@@ -21,7 +21,7 @@ TEMPLATE_DEBUG = DEBUG
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY=os.environ.get("SECRET_KEY", "")
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['biasedgaming.herokuapp.com',]
 
 
 # Application definition
